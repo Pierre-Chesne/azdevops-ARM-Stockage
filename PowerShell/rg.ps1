4fb816e189e22e3ec9643739d4843729928ddfec
Pierre-Chesne <pierrc@hotmail.com>
2020-12-31 16:37:41 +0100

    arm

--- PowerShell/rg.ps1
+++ PowerShell/rg.ps1
@@ -1,5 +1,1 @@
-$rg = Get-AzResourceGroup -name $(rgName) -location $(location)
-if($rg -eq $null)
-{
-  New-AzResourceGroup -name $(rgName) -location $(location)
-}
+New-AzResourceGroup -name 'rg-toto' -location 'westeurope'
