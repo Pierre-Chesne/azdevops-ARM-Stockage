4b7a4bc86e5c32ef44df0c6e6e360654a4ab14c6
Pierre-Chesne <pierrc@hotmail.com>
2020-12-31 17:20:40 +0100

    arm

--- PowerShell/rg.ps1
+++ PowerShell/rg.ps1
@@ -1,5 +1,1 @@
-$rg = Get-AzResourceGroup -Name $(rgName) -location $(location)
-if($rg -eq $null)
-{
-    New-AzResourceGroup -name $(rgName) -location $(location)
-}
+New-AzResourceGroup -name 'rg-test-peter' -location 'westeurope'
